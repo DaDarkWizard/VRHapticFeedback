18eefcdba607f79d8cf82e2c5416f9f83d5a12b7
Dasker <dtmasker@mtu.edu>
2024-03-31 17:27:24 -0400

    Added table texture

--- android/build/assets/scripts/text_display.gd
+++ android/build/assets/scripts/text_display.gd
@@ -4,3 +4,7 @@ extends Label3D
 
 func _on_right_hand_button_pressed(name):
 	self.text = name
+
+
+func _on_folding_table_moved(location):
+	self.text = location.to_string() # Replace with function body.
--- android/build/build/intermediates/assets/debug/mergeDebugAssets/scripts/table_controller.gd
+++ android/build/build/intermediates/assets/debug/mergeDebugAssets/scripts/table_controller.gd
@@ -1,8 +1,9 @@
 extends MeshInstance3D
 
-
+signal moved(location)
 
 func _on_right_hand_button_pressed(button_name):
 	if button_name == "grip_click":
 		self.global_position = get_tree().current_scene\
-			.get_child(0).get_child(2).global_position + Vector3(0, -0.08, 0)
+			.get_child(0).get_child(2).global_position + Vector3(0, -0.5, 0)
+		moved.emit(self.global_position)
